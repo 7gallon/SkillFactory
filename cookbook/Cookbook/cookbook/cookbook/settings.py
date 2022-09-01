458fb7b69c40688fd35970e70ca7028ee9cde8c4
Alexander <G_A_Alexander@mail.ru>
2022-09-01 15:40:59 +0200

    #cookbook (убран django token)

--- cookbook/Cookbook/cookbook/cookbook/settings.py
+++ cookbook/Cookbook/cookbook/cookbook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .seckey import seckey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*&!_c95dbd)37b8@-*achqt1ti20n^yc@shfdqf_7^ee=gv49%'
+SECRET_KEY = seckey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
